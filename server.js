a508dacde69be0ce0355a58306fb7171eb680c1a
Billy Hao <billyhao12@gmail.com>
2020-06-11 20:32:03 -0700

    Completed routes and API routes

--- server.js
+++ server.js
@@ -7,17 +7,40 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
+// Data
+var reservations = [{}];
+var waitlist = [{}];
+
+// API Routes
+app.get("/api/reservations", function(req, res) {
+    return res.json(reservations);
+});
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+});
+
 // Routes
-app.get("/", function(req, res) {
+app.get("/home", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
-  });
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
 
 // Starts the server to begin listening
-app.listen(PORT, function() {
+app.listen(PORT, function () {
 
-  console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT);
 
 });
